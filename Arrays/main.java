9fe8f082026a9ba9f4cc6181df898c721ef05736
mesonkhatiwada <misonkhatiwada098@gmail.com>
2026-01-16 16:55:09 +0545

    Initial commit

---
+++ Arrays/main.java
@@ -0,0 +1,13 @@
+package Arrays;
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        int [] marks=  new int[3] ;
+        marks[0]=100;
+        marks[1]=100;
+        marks[2]=100;
+        for (int i=0;i<3;i++){
+            System.out.println(marks[i]);
+        }
+}
+}
